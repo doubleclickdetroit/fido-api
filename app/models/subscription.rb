96667b4cd5a2bcf662f72b29d69c45f3f1a93152
Ben Babics <bbabics@gmail.com>
2015-09-23 00:52:19 -0400

    Subscription model set initial quantity before create

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,11 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
+
+  before_create :set_initial_quantity
+
+  private
+    def set_initial_quantity
+      products = user.occasions
+      self.quantity = products.count
+    end
 end
