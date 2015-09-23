3db00c352220fcd2248a2abf08a49d3569416f9d
Ben Babics <bbabics@gmail.com>
2015-09-23 00:51:54 -0400

    Validate Occasion.date is greater than today

--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -1,4 +1,12 @@
 class Occasion < ActiveRecord::Base
   belongs_to :user
   belongs_to :contact
+
+  before_save :ensure_date_is_future
+
+  private
+    def ensure_date_is_future
+      today = Date.today
+      today < self.date
+    end
 end
